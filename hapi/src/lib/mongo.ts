c3b8f5f8d22d11afbb369fb9e6ba4cea2e850b00
Niño Arenas <ntb.arenas@protonmail.com>
2023-04-05 10:55:31 +0100

    Update mongodb service

--- hapi/src/lib/mongo.ts
+++ hapi/src/lib/mongo.ts
@@ -1,38 +1,37 @@
-import Hapi from '@hapi/hapi'
-import hapiMongo from 'hapi-mongodb'
+import Hapi from "@hapi/hapi";
+import hapiMongo from "hapi-mongodb";
 
 /**
  * `mongo@1.0.0`
  * - Register the 'hapi-mongodb'
  */
 export default Object.freeze<Hapi.Plugin<void>>({
-  name: 'mongo',
-  version: '1.0.0',
-  register: async server => {
-
-    const usr = '--'
-    const pwd = '--'
+  name: "mongo",
+  version: "1.0.0",
+  register: async (server) => {
+    const usr = "edit-backend";
+    const pwd = "KJuvZX08zaIxhjEz";
     const options: hapiMongo.Options = {
-      url: `mongodb+srv://${usr}:${pwd}@edit-backend-jan-2023.6j9py79.mongodb.net/sample_mflix?retryWrites=true&w=majority`,
-      settings: {useUnifiedTopology: true},
+      url: `mongodb+srv://${usr}:${pwd}@edit-full-stack.6hbxvza.mongodb.net/backend-project?retryWrites=true&w=majority`,
+      settings: { useUnifiedTopology: true },
       decorate: true,
-    }
+    };
 
     await server.register({
       plugin: hapiMongo,
       options,
-    })
+    });
   },
-})
+});
 
-declare module '@hapi/hapi' {
+declare module "@hapi/hapi" {
   // extends typeof `Hapi.Server`
   interface Server {
-    mongo: hapiMongo.HapiMongo
+    mongo: hapiMongo.HapiMongo;
   }
 
   // extends typeof `Hapi.Request`
   interface Request {
-    mongo: hapiMongo.HapiMongo
+    mongo: hapiMongo.HapiMongo;
   }
 }
